3362330331f4fcb5c6a94b1bdb397579cef07ff6
James Mead <james@floehopper.org>
2011-12-20 17:28:41 +0000

    Allow rummager host to be overridden.
    - We need to point Rummager within the Whitehall app at the
      whitehall rummager instance and not the citizen rummager instance.
    - There's probably a better way of doing this, but this'll do for now.

--- lib/rummageable.rb
+++ lib/rummageable.rb
@@ -7,6 +7,11 @@ module Rummageable
   InvalidDocument = Class.new(RuntimeError)
   CHUNK_SIZE = 20
 
+  attr_writer :host
+  def host
+    @host || Plek.current.find("search")
+  end
+
   # documents must be either a hash (for one document) or an array of hashes
   # (for multiple documents)
   #
@@ -15,7 +20,7 @@ module Rummageable
     documents.each do |document|
       validate_structure document
     end
-    url = Plek.current.find("search") + "/documents"
+    url = host + "/documents"
     0.step(documents.length - 1, CHUNK_SIZE).each do |i|
       body = JSON.dump(documents[i, CHUNK_SIZE])
       RestClient.post url, body, content_type: :json, accept: :json
@@ -23,7 +28,7 @@ module Rummageable
   end
 
   def delete(link)
-    url = Plek.current.find("search") + "/documents/" + CGI.escape(link)
+    url = host + "/documents/" + CGI.escape(link)
     RestClient.delete url, content_type: :json, accept: :json
   end
 
