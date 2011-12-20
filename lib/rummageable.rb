fbe8927c17f47a3cd0bdedb10527f452108f633e
James Mead <james@floehopper.org>
2011-12-20 17:55:46 +0000

    Allow addition of a path prefix.
    We need this for the whitehall instance of rummager.

--- lib/rummageable.rb
+++ lib/rummageable.rb
@@ -12,6 +12,11 @@ module Rummageable
     @host || Plek.current.find("search")
   end
 
+  attr_writer :path_prefix
+  def path_prefix
+    @path_prefix || ""
+  end
+
   # documents must be either a hash (for one document) or an array of hashes
   # (for multiple documents)
   #
@@ -20,7 +25,7 @@ module Rummageable
     documents.each do |document|
       validate_structure document
     end
-    url = host + "/documents"
+    url = host + path_prefix + "/documents"
     0.step(documents.length - 1, CHUNK_SIZE).each do |i|
       body = JSON.dump(documents[i, CHUNK_SIZE])
       RestClient.post url, body, content_type: :json, accept: :json
@@ -28,7 +33,7 @@ module Rummageable
   end
 
   def delete(link)
-    url = host + "/documents/" + CGI.escape(link)
+    url = host + path_prefix + "/documents/" + CGI.escape(link)
     RestClient.delete url, content_type: :json, accept: :json
   end
 
