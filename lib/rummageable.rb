0bd6988e7c927cfc1615f0c709f2d277e91004cc
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-07-03 10:30:34 +0100

    wip

--- lib/rummageable.rb
+++ lib/rummageable.rb
@@ -1,74 +1,76 @@
-require "rest_client"
-require 'multi_json'
-require "plek"
-
-require "rummageable/implementation"
-require "rummageable/fake"
+require 'benchmark'
+require 'null_logger'
 
 module Rummageable
+  attr_reader :name
 
-  InvalidDocument = Class.new(RuntimeError)
-  CHUNK_SIZE = 20
+  class Connection
+    def initialize(rummager_url, name, options = {})
+      @name = name
+      @rummager_url = rummager_url.sub(%r{/$}, "")
+      @logger = options[:logger] || NullLogger.instance
+      @kernel = options[:kernel] || Kernel
+      @retries = options[:retries] || 3
+      @batch_size = options[:batch_size] || 100
+    end
 
-  attr_writer :rummager_service_name
-  def rummager_service_name
-    @rummager_service_name || "search"
+    def index(name)
+      Index.new(self, name)
+    end
   end
 
-  attr_writer :rummager_host
-  def rummager_host
-    @rummager_host || Plek.current.find(rummager_service_name)
+  class Index
+    def initialize(connection, name)
+      @connection = connection
+    end
+
+    def add(entry)
+      repeatedly do
+        post_batch [entry]
+      end
+    end
+
+    def add_batch(entries)
+      entries.each_slice(@batch_size) do |batch|
+        repeatedly do
+          post_batch(batch)
+        end
+      end
+    end
+
+    def delete(link)
+    end
+
+    def commit
+    end
+
+  private
+    def post_batch(batch)
+      response = nil
+      @logger.info "Posting #{batch.size} document(s) to #{rummager_endpoint}"
+      @logger.debug batch.map {|entry| entry['link']}.join(", ")
+      call_time = Benchmark.realtime do
+        response = RestClient.post(rummager_endpoint, MultiJson.encode(batch), content_type: :json, accept: :json)
+      end
+      @logger.debug "Response: #{response} took (#{call_time})"
+      response
+    end
+
+    def rummager_endpoint
+      "#{@rummager_url}/#{@name}/documents"
+    end
+
+    def repeatedly(&block)
+      (1..@retries).each do |i|
+        begin
+          return yield
+        rescue RestClient::RequestFailed => e
+          @logger.warn e
+          raise if i == @retries
+          @logger.info "Retrying... attempt #{i}"
+          @kernel.sleep(5 * i)
+        end
+      end
+    end
   end
-
-  attr_writer :default_index
-  def default_index
-    @default_index || ""
-  end
-
-  attr_writer :implementation
-  def implementation
-    @implementation ||= Implementation.new
-  end
-
-  # documents must be either a hash (for one document) or an array of hashes
-  # (for multiple documents)
-  #
-  def index(documents, index_name = default_index)
-    implementation.index(documents, index_name)
-  end
-
-  def delete(link, index_name = default_index)
-    implementation.delete(link, index_name)
-  end
-
-  def delete_all(index_name = default_index)
-    implementation.delete_all(index_name)
-  end
-
-  def amend(link, amendments, index_name = default_index)
-    implementation.amend(link, amendments, index_name)
-  end
-
-  def commit(index_name = default_index)
-    implementation.commit(index_name)
-  end
-
-  VALID_KEYS = [
-    %w[title],
-    %w[description],
-    %w[format],
-    %w[section],
-    %w[subsection],
-    %w[subsubsection],
-    %w[link],
-    %w[indexable_content],
-    %w[boost_phrases],
-    %w[link_order],
-  ]
-
-  def validate_structure(hash, parents=[])
-    implementation.validate_structure(hash, parents)
-  end
-
-  extend self
 end
